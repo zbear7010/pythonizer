4c16ef49badfe89f107fe3f76c953164dbd29051
zbear7010 <zbear0107@gmail.com>
2022-10-24 21:31:01 +0400

    git push

---
+++ asyncro.py
@@ -0,0 +1,25 @@
+import socket
+
+# domain:5000
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind(('localhost', 5000))
+server_socket.listen()
+
+while True:
+    print('Before .accept()')
+    client_socket, addr = server_socket.accept()
+    print('Connection from', addr)
+
+    while True:
+        print('Before .recv()')
+        request = client_socket.recv(4096)
+
+        if not request:
+            break
+        else:
+            response = 'Hello, world\n'.encode()
+            client_socket.send(response)
+
+    print('Жду нового коннекта')
